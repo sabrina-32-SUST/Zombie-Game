dd8ef682469bdd156eecb14dde8d2c8f37294fed
Sabrina <sanutaayvi@gmail.com>
2022-03-20 20:13:08 +0600

    rendaring object into screen

--- src/org/graphics/EventListener.java
+++ src/org/graphics/EventListener.java
@@ -1,5 +1,6 @@
 package org.graphics;
 
+import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLEventListener;
 
@@ -7,17 +8,27 @@ public class EventListener implements GLEventListener {
 
 	@Override
 	public void display(GLAutoDrawable drawable) {
-		System.out.println("Hello world");
 
+	GL2 gl = drawable.getGL().getGL2();
+	gl.glClear(GL2.GL_COLOR_BUFFER_BIT);
+	gl.glColor3f(0, 0, 1);
+	gl.glBegin(GL2.GL_QUADS);
+	gl.glVertex2f(-0.5f, -0.5f);
+	gl.glVertex2f(0.5f, -0.5f);
+	gl.glVertex2f(0.5f, 0.5f);
+	gl.glVertex2f(-0.5f, 0.5f);
+gl.glEnd();
+	
 	}
-
+ 
 	@Override
 	public void dispose(GLAutoDrawable implement) {
 		
 	}
 
 	@Override
-	public void init(GLAutoDrawable implement) {
+	public void init(GLAutoDrawable drawable) {
+	
 		
 	}
 
